173f1b1cef07ec11a6f4f5617e440551c3e457ac
Kyle Butler <54778108+kyle9021@users.noreply.github.com>
2022-06-09 11:01:13 -0400

    Update etl.sh

--- etl.sh
+++ etl.sh
@@ -52,8 +52,57 @@ CONFIG_RESPONSE=$(curl --request POST \
 
 quick_check "/search/config"
 
+printf '%s' "$CONFIG_RESPONSE" > './json/temp_config.json'
+VULN_SEARCH=$(cat <<EOF
+{
+  "query":"config from cloud.resource where api.name = 'aws-ec2-describe-instances' AND finding.type IN ( 'Host Vulnerability' )",
+  "timeRange":{
+     "type":"relative",
+     "value":{
+        "unit":"hour",
+        "amount":24
+     }
+  }
+}
+EOF
+)
+
+VULN_RESPONSE=$(curl --request POST \
+                       --url "$PC_APIURL/search/config" \
+                       --header 'content-type: application/json; charset=UTF-8' \
+                       --header "x-redlock-auth: $PC_JWT" \
+                       --data "$VULN_SEARCH")
+
+quick_check "/search/config"
+
 
 JSON_LOCATION=./json
+VULN_RRN_ARRAY=( $(printf '%s' "$VULN_RESPONSE" | jq -r '.data.items[].rrn') )
+
+
+for rrn in "${VULN_RRN_ARRAY[@]}"; do \
+VULN_PAYLOAD=$(cat <<EOF
+{
+ "rrn": "$rrn",
+ "findingType":[],
+ "riskFactors":[]
+}
+EOF
+)
+
+
+VULN_FINDINGS=$(curl --request POST \
+                     --url "$PC_APIURL/resource/external_finding" \
+                     --header 'content-type: application/json; charset=UTF-8' \
+                     --header 'accept: application/json, text/plain, */*' \
+                     --header "x-redlock-auth: $PC_JWT" \
+                     --data-raw "$VULN_PAYLOAD")
+
+quick_check "/resource/external_finding"
+
+printf '%s' "$VULN_FINDINGS" | jq '.[] | {normalizedName: .normalizedName, riskFactors: .riskFactors, findingId: .findingId, source: .source, severity: .severity, status: .status, resourceCloudId: .resourceCloudId}' >> "$JSON_LOCATION/temp_vuln.json"
+
+done
 
 #printf '%s' "$CONFIG_RESPONSE" > $JSON_LOCATION/temp_vm.json
 
@@ -82,16 +131,24 @@ IAM_QUERY_RESPONSE=$(curl --request POST \
                           --data "$IAM_QUERY")
 quick_check "/api/v1/permission"
 
+
 # dumps the IAM query response to a temp json file
 printf '%s' "$IAM_QUERY_RESPONSE" |jq '.data.items[]' >> "$JSON_LOCATION/temp_iam.json"
+
+
 done
 
+# combines the responses from the iam query and the config query on the name of the ec2 instance and the .sourceResourceName from the iam query. starts the transform
+printf '%s' "$CONFIG_RESPONSE" | jq '[.data.items[] | {id: .id, name: .name, uid: ("_:" + .name), rrn: .rrn, imageId: .data.imageId, state: .data.state, licenses: .data.licenses, tags: .data.tags, networkInterfaces: .data.networkInterfaces, blockDeviceMappings: .data.blockDeviceMappings} ]| map({id, name, uid, rrn, imageId, state, licenses, tags, networkInterfaces, blockDeviceMappings, iam: [(.name as $name | $iamdata |..| select(.sourceResourceName? and .sourceResourceName==$name))]})' --slurpfile iamdata "$JSON_LOCATION/temp_iam.json" |\
 
-# combines the two responses together on the name of the vm and the name of the source permissions, then does a transform before loading into dgraph
-printf '%s' "$CONFIG_RESPONSE" | jq '[.data.items[] | {id: .id, name: .name, uid: ("_:" + .name), rrn: .rrn, imageId: .data.imageId, state: .data.state, licenses: .data.licenses, tags: .data.tags, networkInterfaces: .data.networkInterfaces, blockDeviceMappings: .data.blockDeviceMappings} ]| map({id, name, uid, rrn, imageId, state, licenses, tags, networkInterfaces, blockDeviceMappings, iam: [(.name as $name | $iamdata |..| select(.sourceResourceName? and .sourceResourceName==$name))]})' --slurpfile iamdata "$JSON_LOCATION/temp_iam.json" | jq '[.[] | {id: .id, name: .name, uid: .uid, rrn: .rrn, uid2: ("_:" + .rrn), imageId: .imageId, uid3: ("_:" + .imageId), networkInterfaces: [.networkInterfaces[] | {vpcId: .vpcId, uid4: ("_:" + .vpcId), groups: [.groups[] | {groupId: .groupId, uid5: ("_:" + .groupId), groupName: .groupName, uid6: ("_:" + .groupName)} ], status: .status, ownerId: .ownerId, uid7: ("_:" + .ownerId), attachment: {status: .attachment.status, attachTime: .attachment.attachTime, attachmentId: .attachment.attachmentId, uid8: ("_:" + .attachment.attachmentId), networkCardIndex: .attachment.networkCardIndex, deleteOnTermination: .attachment.deleteOnTermination}, macAddress: .macAddress, uid9: ("_:" + .macAddress), interfaceType: .interfaceType, ipv6Addresses: .ipv6Addresses , privateDnsName: .privateDnsName, uid10: ("_:" + .privateDnsName), sourceDestCheck: .sourceDestCheck, privateIpAddress: .privateIpAddress, networkInterfaceId: .networkInterfaceId, uid11: ("_:" + .networkInterfaceId ), privateIpAddresses: [.privateIpAddresses[] | {primary: .primary, privateDnsName: .privateDnsName, privateIpAddress: .privateIpAddress, association: {publicIp: .association.publicIp?, ipOwnerId: .association.ipOwnerId?, publicDnsName: .association.publicDnsName?}} ] }], blockDeviceMappings: [.blockDeviceMappings[] | {ebs: {status: .ebs.status, volumeId: .ebs.volumeId, uid12: ("_:" + .ebs.volumeId), attachTime: .ebs.attachTime, deleteOnTermination: .ebs.deleteOnTermination}} ], iam: .iam} ] | {set: . }' > "$JSON_LOCATION/done.json"
+jq '[.[] | {id: .id, name: .name, uid: .uid, rrn: .rrn, uid2: ("_:" + .rrn), imageId: .imageId, uid3: ("_:" + .imageId), networkInterfaces: [.networkInterfaces[] | {vpcId: .vpcId, uid4: ("_:" + .vpcId), groups: [.groups[] | {groupId: .groupId, uid5: ("_:" + .groupId), groupName: .groupName, uid6: ("_:" + .groupName)} ], status: .status, ownerId: .ownerId, uid7: ("_:" + .ownerId), attachment: {status: .attachment.status, attachTime: .attachment.attachTime, attachmentId: .attachment.attachmentId, uid8: ("_:" + .attachment.attachmentId), networkCardIndex: .attachment.networkCardIndex, deleteOnTermination: .attachment.deleteOnTermination}, macAddress: .macAddress, uid9: ("_:" + .macAddress), interfaceType: .interfaceType, ipv6Addresses: .ipv6Addresses , privateDnsName: .privateDnsName, uid10: ("_:" + .privateDnsName), sourceDestCheck: .sourceDestCheck, privateIpAddress: .privateIpAddress, networkInterfaceId: .networkInterfaceId, uid11: ("_:" + .networkInterfaceId ), privateIpAddresses: [.privateIpAddresses[] | {primary: .primary, privateDnsName: .privateDnsName, privateIpAddress: .privateIpAddress, association: {publicIp: .association.publicIp?, ipOwnerId: .association.ipOwnerId?, publicDnsName: .association.publicDnsName?}} ] }], blockDeviceMappings: [.blockDeviceMappings[] | {ebs: {status: .ebs.status, volumeId: .ebs.volumeId, uid12: ("_:" + .ebs.volumeId), attachTime: .ebs.attachTime, deleteOnTermination: .ebs.deleteOnTermination}} ], iam: .iam} ]' > "$JSON_LOCATION/temp_config_iam.json"
+
+
+# combines the three responses together and merges the json on the id from the config response and the resourceCloudId on the vulnerability response
+cat "$JSON_LOCATION/temp_config_iam.json" | jq '. | map({id, name,uid, rrn, uid2,imageId, uid3, networkInterfaces, blockDeviceMappings, iam, vulnerability: [(.id as $id | $vulndata |..| select( .resourceCloudId? and .resourceCloudId==$id ))]})' --slurpfile vulndata "$JSON_LOCATION/temp_vuln.json" | jq '{set: .}' > "$JSON_LOCATION/done.json"
 
 # fixes the key value pairs getting it ready for import to dgraph
-sed -i 's/uid[0-9]\{0,3\}/uid/g' "$JSON_LOCATION/done.json"
+sed -i 's/uid[0-9]{0,9}/uid/g' "$JSON_LOCATION/done.json"
 
 
 # load the data into the alpha mutate endpoint
@@ -99,6 +156,8 @@ curl -H "content-type: application/json" \
      -X POST \
      --url "localhost:8080/mutate?commitNow=true" \
      --data-binary @"$JSON_LOCATION/done.json"
+
+
 quick_check "/mutate?/commitNow=true"
 
 
